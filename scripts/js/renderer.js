2c78ecee3266d246a00c6c5208d8aef5e927be68
Mahesh Manni <mahesh.manni@gmail.com>
2021-11-16 17:45:53 +0530

    updated to new template

--- scripts/js/renderer.js
+++ scripts/js/renderer.js
@@ -1,52 +1,221 @@
+class SceneObjects
+{
+    constructor(object, material, mesh)
+    {
+        this.object = object;
+        this.material = material;
+        this.mesh = mesh;
+    }
+}
+var material;
+var selectedObjColor;
+var hasObjectsPicked = false;
+const aSceneObjects = [];
+const aBackgroundScene = [];
+var renderer;
 function Render() {
     const scene = new THREE.Scene();
     var gui = new dat.GUI();
     const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-    const renderer = new THREE.WebGLRenderer();
-    //renderer.setClearColorHex(0xEEEEEE);
+    renderer = new THREE.WebGLRenderer();
+    renderer.setClearColor(new THREE.Color( "rgb(128, 128, 128)"), 1.0);
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
 
     const geometry = new THREE.BoxGeometry();
-    const material = getMaterial('standard', 'rgb(255, 217, 145)');
+   // = getMaterial('standard', 'rgb(255, 217, 145)');
     const cube = new THREE.Mesh( geometry, material );
    cube.castShadow = true;
     cube.position.y = cube.geometry.parameters.height/2;
     //scene.add( cube );
 	
+    document.addEventListener('mousedown', onDocumentMouseDown, false);
+    function onDocumentMouseDown(event) {
+
+        console.log("mouse down");
+        const _y = event.clientY - 50;
+        var vector = new THREE.Vector3(( event.clientX / window.innerWidth ) * 2 - 1, -( _y / window.innerHeight ) * 2 + 1, 0.5);
+            vector = vector.unproject(camera);
+
+            var raycaster = new THREE.Raycaster(camera.position, vector.sub(camera.position).normalize());
+            const objectsToIntersect = [];
+            var index = 0;
+            while(index < aSceneObjects.length)
+            {
+                objectsToIntersect.push(aSceneObjects[index].mesh);
+                index++;
+            }
+            var intersects = raycaster.intersectObjects(objectsToIntersect);
+
+            if(hasObjectsPicked == true)
+            {
+                material.color = selectedObjColor;
+            }
+            hasObjectsPicked = false;
+            if (intersects.length > 0) {
+
+                material = intersects[0].object.material;
+                selectedObjColor = material.color;
+                material.color = new THREE.Color( "rgb(255, 255, 0)");
+                hasObjectsPicked = true;
+            }
+    }
     var fileButton = document.getElementById("fileButton");
     fileButton.addEventListener('change', function(e){
 
     var file = e.target.files[0];
     const fileExt = GetFileExtension(file.name);
+
+  
     if(fileExt == 'obj' || fileExt == 'OBJ')
     {
+        material = getMaterial('standard', 'rgb(255, 217, 145)');
+        material.envMap = reflectionCube;
         var loader = new THREE.OBJLoader();
         var reader = new FileReader();
-    reader.readAsText(file);
-    reader.addEventListener('load', function(event) {
+        reader.readAsText(file);
+        reader.addEventListener('load', function(event) {
       var contents = event.target.result;
       var object = loader.parse(contents);
-
-      object.traverse(function(child) {
+      
+      if(object.children.length > 0)
+      {
+          const child = object.children[0];
         child.material = material;
-      } );
+        const sceneObj = new SceneObjects(object, material, child);
+        aSceneObjects.push(sceneObj);
+      }
+      //currentObject = object;
+   /*   object.traverse(function(child) {
+        child.material = material;
+        const sceneObj = new SceneObjects(object, material, new THREE.Mesh(child.geometry));
+      aSceneObjects.push(sceneObj);
+      } );*/
       scene.add(object);
       });
     }
     
     else
     {
+        material = getMaterial('standard', 'rgb(255, 217, 145)');
+        material.envMap = reflectionCube;
+
         var loader = new THREE.STLLoader();
         var tmppath = URL.createObjectURL(e.target.files[0]);
-      loader.load( tmppath, function ( geometry ) {
-        scene.add( new THREE.Mesh( geometry, material ) );
+        loader.load( tmppath, function ( geometry ) {
+        var mesh = new THREE.Mesh(geometry);
+        mesh.material = material;
+        const sceneObj = new SceneObjects(mesh, material, mesh);
+        aSceneObjects.push(sceneObj);
+        scene.add(mesh);  
+        
         });
     }
       
 
 });
+
+const colorPickElemnent = document.getElementById("colorpicker");
+colorPickElemnent.addEventListener('change', function(e) {
+
+scene.background = 0;
+renderer.setClearColor(colorPickElemnent.value, 1.0);
+});
+
+document.getElementById("mySidenav").addEventListener('click', function(e) {
+  
+    
+    switch(e.target.id) {
+        case '0':
+            material.color = new THREE.Color( "rgb(255, 217, 145)");
+            material.opacity = 1.0;
+                material.transparent = false;
+          break;
+        case '1':
+            
+            material.color = new THREE.Color( "rgb(97%, 96%, 91%)" );
+            material.opacity = 1.0;
+                material.transparent = false;
+          break;
+        case '2':
+            
+            material.color = new THREE.Color( "rgb(80%, 49%, 19%)" );
+            material.opacity = 1.0;
+                material.transparent = false;
+            break;
+        case '3':
+            material.color = new THREE.Color( 'red' );
+            material.opacity = 1.0;
+                material.transparent = false;
+            break;
+        case '4':
+            material.color = new THREE.Color( 'blue' );
+            material.opacity = 1.0;
+            material.transparent = false;
+            break;
+        case '5':
+            material.color = new THREE.Color( 'grey' );
+            material.opacity = 1.0;
+            material.transparent = false;
+            break;
+            case '6':
+                
+                material.color = new THREE.Color( "rgb(0, 255, 0)");
+                material.opacity = 0.8;
+                material.transparent = true;
+              break;
+            case '7':
+                
+                material.color = new THREE.Color( "rgb(0, 0, 255)");
+                material.opacity = 0.8;
+                material.transparent = true;
+              break;
+            case '8':
+                
+                material.color = new THREE.Color( "rgb(255, 0, 0)");
+                material.opacity = 0.8;
+                material.transparent = true;
+                break;
+            case '9':
+                material.color = new THREE.Color( 'white' );
+                material.opacity = 0.8;
+                material.transparent = true;
+                break;
+            case '10':
+                material.color = new THREE.Color( 'yellow' );
+                material.opacity = 0.8;
+                material.transparent = true;
+                break;
+            case '11':
+                    Undo(scene);
+                break;
+            case '12':
+                    DeleteAll(scene);
+                break;
+            case '13':
+                scene.background = aBackgroundScene[0];
+                break;
+            case '14':
+                scene.background = aBackgroundScene[1];
+                break;
+            case '15':
+                scene.background = aBackgroundScene[2];
+                break;
+            case '16':
+                scene.background = aBackgroundScene[3];
+                break;
+            case '17':
+                scene.background = aBackgroundScene[4];
+                break;
+                  
+        default:
+            break;
+      }
+   
+    return false; // avoiding navigation
+});
+
 
 	/*loader.load('https://firebasestorage.googleapis.com/v0/b/manni-technology.appspot.com/o/folder%2Flee-perry-smith-head-scan.obj?alt=media&token=54981eee-fa05-4dc2-a490-2ee006de0c78', function (object) {
 
@@ -60,8 +229,8 @@ function Render() {
     pointLight.position.y = 2;
     pointLight.position.x = 2;
     pointLight.position.z = 2;
-    gui.add(pointLight, 'intensity', 1, 10);
-    gui.add(pointLight.position, 'y', 0, 20);
+  //  gui.add(pointLight, 'intensity', 1, 10);
+    //gui.add(pointLight.position, 'y', 0, 20);
    // gui.add(material, 'shininess', 0, 1000);
    // scene.add(pointLight);
    // scene.add(pointLight);
@@ -70,23 +239,21 @@ function Render() {
     plane.rotation.x = Math.PI/2;
   //  scene.add(plane);
     var axes = THREE.AxisHelper(20);
-    scene.add(axes);
-	gui.add(axes, 'visible');
-	var path = '/assets/cubemap/';
+   // scene.add(axes);
+	//gui.add(axes, 'visible');
+	/*var path = '/assets/cubemap/';
 	var ext = '.jpg';
 
 	var urls = [ path + 'posx' + ext, path + 'negx' + ext,
 				path + 'posy' + ext, path + 'negy' + ext,
 				path + 'posz' + ext, path + 'negz' + ext ];
   var reflectionCube = new THREE.CubeTextureLoader().load(urls);
-    reflectionCube.format = THREE.RGBFormat;
-    scene.background = reflectionCube;
-	material.metalness = 1.0;
-	material.roughness = 0.3;
+    reflectionCube.format = THREE.RGBFormat;*/
+    
+    LoadCubemap();
+    const reflectionCube = aBackgroundScene[0];
+    //scene.background = reflectionCube;
 
-	gui.add(material, 'metalness', 0, 1);
-	gui.add(material, 'roughness', 0, 1);
-	material.envMap = reflectionCube;
     camera.position.z = 5;
     const controls = new THREE.OrbitControls( camera, renderer.domElement );
     controls.update();
@@ -118,6 +285,29 @@ function CreateGroundPlane(size) {
     return mesh;
 }
 
+function Undo(scene)
+{
+    if(aSceneObjects.length > 0)
+    {
+
+        scene.remove(aSceneObjects[aSceneObjects.length - 1].object);
+        aSceneObjects.pop();  
+        if(aSceneObjects.length > 0)
+        {
+            material = aSceneObjects[aSceneObjects.length - 1].material;
+        } 
+    }
+}
+
+function DeleteAll(scene)
+{
+    while(aSceneObjects.length > 0)
+    {
+        scene.remove(aSceneObjects[aSceneObjects.length - 1].object);
+        aSceneObjects.pop();
+    }
+}
+
 function CreatePointLight(internsity) {
 
     var light  = new THREE.PointLight(0xffffff, internsity);
@@ -132,7 +322,7 @@ function CreateDirectionLight(internsity) {
     return light;
 }
 
-function getMaterial(type, color) {
+function getMaterial(type, color, reflectionCube) {
 	var selectedMaterial;
 	var materialOptions = {
 		color: color === undefined ? 'rgb(255, 255, 255)' : color,
@@ -150,6 +340,12 @@ function getMaterial(type, color) {
 			break;
 		case 'standard':
 			selectedMaterial = new THREE.MeshStandardMaterial(materialOptions);
+            selectedMaterial.metalness = 1.0;
+            selectedMaterial.roughness = 0.0;
+        
+            //gui.add(material, 'metalness', 0, 1);
+            //gui.add(material, 'roughness', 0, 1);
+          
 			break;
 		default:
 			selectedMaterial = new THREE.MeshBasicMaterial(materialOptions);
@@ -159,7 +355,35 @@ function getMaterial(type, color) {
 	return selectedMaterial;
 }
 
+function ClearSelection()
+{
+
+}
+
+function SelectObject()
+{
+
+}
+
 function GetFileExtension(filePath)
 {
     return filePath.split('.').pop();
+}
+
+function LoadCubemap()
+{
+    for(var index = 0; index < 5; ++index)
+    {
+        var path = '/assets/cubemap/';
+        path += (index + 1) + '/';
+        var ext = '.png';
+    
+        var urls = [ path + 'px' + ext, path + 'nx' + ext,
+                    path + 'py' + ext, path + 'ny' + ext,
+                    path + 'pz' + ext, path + 'nz' + ext ];
+      var reflectionCube = new THREE.CubeTextureLoader().load(urls);
+        reflectionCube.format = THREE.RGBFormat;
+        aBackgroundScene.push(reflectionCube);
+    }
+    
 }
